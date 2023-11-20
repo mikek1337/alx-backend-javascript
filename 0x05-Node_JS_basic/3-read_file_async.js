62d75fc52088c74fdb6eaf1b2831f0c2122a816e
mikek1337 <mikekegnlyew@gmail.com>
2023-11-20 14:56:43 +0300

    read file in async

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+module.exports = async (path) => {
+  const data = await fs.readFileSync(path);
+  if (!data) {
+    console.log("Cannot load the database");
+  } else {
+    const lines = data.toString().split("\n");
+    lines.shift();
+    console.log(`Number of students: ${lines.length}`);
+    let fieldsCounts = {};
+    for (let line in lines) {
+      const entry = lines[line].split(",");
+      if (fieldsCounts[entry[entry.length - 1]] != null) {
+        if (entry[entry.length - 1] !== "") {
+          fieldsCounts[entry[entry.length - 1]].count++;
+          fieldsCounts[entry[entry.length - 1]].names.push(entry[0]);
+        }
+      } else {
+        if (entry[entry.length - 1] !== "") {
+          fieldsCounts[entry[entry.length - 1]] = {
+            count: 1,
+            names: [entry[0]],
+          };
+        }
+      }
+    }
+    for (let field in fieldsCounts) {
+      console.log(
+        `Number of students in ${field}: ${
+          fieldsCounts[field].count
+        }. List: ${fieldsCounts[field].names.join(", ")}`
+      );
+    }
+  }
+};
