0de52c7a27ebcc924279045c1522586090a01f7d
mikek1337 <mikekegnlyew@gmail.com>
2023-11-21 20:57:24 +0300

    http server with route

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,11 +1,11 @@
-const http = require("http");
-const countStudents = require("./3-read_file_async");
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-  if (req.url === "/students") {
-    countStudents("database.csv");
+  if (req.url === '/students') {
+    countStudents('database.csv');
   } else {
-    res.end("Hello Holberton School!");
+    res.end('Hello Holberton School!');
   }
 });
 
