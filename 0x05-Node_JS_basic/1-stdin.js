172c71c84c83019a1db83d33c2a4c74bf8304139
mikek1337 <mikekegnlyew@gmail.com>
2023-11-21 19:25:39 +0300

    fixed code format

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 function input() {
-  console.log("Welcome to Holberton School, what is your name?");
-  process.stdin.on("readable", () => {
-    let data;
-    if ((data = process.stdin.read()) !== null) {
+  console.log('Welcome to Holberton School, what is your name?');
+  process.stdin.on('readable', () => {
+    const data = process.stdin.read();
+    if (data !== null) {
       process.stdout.write(`Your name is: ${data}`);
     }
   });
-  process.stdin.on("end", (code) => {
-    console.log("This important software is now closing");
+  process.stdin.on('end', () => {
+    console.log('This important software is now closing');
   });
 }
 
