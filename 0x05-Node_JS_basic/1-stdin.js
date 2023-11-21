8f57ab62b1b1dd080d7464288b10ca91c44d4029
mikek1337 <mikekegnlyew@gmail.com>
2023-11-21 19:03:51 +0300

    changed console.log to process output

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 function input() {
   console.log("Welcome to Holberton School, what is your name?");
-  const input = process.stdin.on("readable", () => {
+  process.stdin.on("readable", () => {
     let data;
     if ((data = process.stdin.read()) !== null) {
-      console.log(`Your name is: ${data}`);
+      process.stdout.write(`Your name is: ${data}`);
     }
   });
-  process.on("exit", (code) => {
+  process.stdin.on("end", (code) => {
     console.log("This important software is now closing");
   });
 }
