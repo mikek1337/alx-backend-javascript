949ae4fd96d939a27e2ce103dbd0792e7465701e
mikek1337 <mikekegnlyew@gmail.com>
2023-11-23 19:09:10 +0300

    improved type

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,4 +1,4 @@
-const calculateNumber = (a, b, type) => {
+const calculateNumber = (type, a, b) => {
   if (type === "SUM") {
     return Math.round(a) + Math.round(b);
   }
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,28 +1,36 @@
-const assert = require("assert");
-const calculateNumber = require("./1-calcul");
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
-describe("calculateNumber", () => {
-  it("should return the sum of two integers", () => {
-    assert.strictEqual(calculateNumber(1, 2, "SUM"), 3);
-    assert.strictEqual(calculateNumber(5, 5, "SUM"), 10);
-    assert.strictEqual(calculateNumber(-5, -5, "SUM"), -10);
+describe('calculateNumber', () => {
+  it('floating point whole numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
   });
 
-  it("should return the difference of two integers", () => {
-    assert.strictEqual(calculateNumber(5, 2, "SUBTRACT"), 3);
-    assert.strictEqual(calculateNumber(10, 5, "SUBTRACT"), 5);
-    assert.strictEqual(calculateNumber(-5, -5, "SUBTRACT"), 0);
+  it('rounding down b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
   });
 
-  it("should return the division of two integers", () => {
-    assert.strictEqual(calculateNumber(10, 2, "DIVIDE"), 5);
-    assert.strictEqual(calculateNumber(5, 10, "DIVIDE"), 0.5);
-    assert.strictEqual(calculateNumber(-10, -5, "DIVIDE"), 2);
+  it('rounding down a and b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
   });
 
-  it('should return "Error" when dividing by 0', () => {
-    assert.strictEqual(calculateNumber(10, 0, "DIVIDE"), "Error");
-    assert.strictEqual(calculateNumber(-5, 0, "DIVIDE"), "Error");
-    assert.strictEqual(calculateNumber(0, 0, "DIVIDE"), "Error");
+  it('rounding down a\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+  });
+
+  it('rounding up b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
+  });
+
+  it('rounding up a and b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
+  });
+
+  it('rounding up a\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+  });
+
+  it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
 });
